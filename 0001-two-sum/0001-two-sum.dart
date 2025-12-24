a19af2f8e4067d99d6a285f4aa441a2f0fdea4a6
fathimashameema <fathimashameema116@gmail.com>
2025-12-24 22:55:37 +0530

    Time: 85 ms (16.39%), Space: 152.4 MB (8.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.dart
+++ 0001-two-sum/0001-two-sum.dart
@@ -1,14 +1,15 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    List<int> output =[];
+    // List<int> output =[];
     for(int i=0;i<nums.length;i++){
         for(int j=i+1;j<nums.length;j++){
             if(nums[i]+nums[j]==target){
-                output.add(i);
-                output.add(j);
+                // output.add(i);
+                // output.add(j);
+                return [i,j];
             }
         }
     }
-    return output;
+    return [];
   }
 }
